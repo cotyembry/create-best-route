930a8a79790004fd388dfb5d8592a9b4bc0fbad1
cotyembry <cotyembry@gmail.com>
2017-07-08 23:02:23 -0500

    added file reading logic

--- js/Components/TakePicture.jsx
+++ js/Components/TakePicture.jsx
@@ -8,22 +8,54 @@ import React from 'react';
 import Text from './Text.jsx';
 
 
+import { handleFileSelect, fileReader } from '../fileReader.js';
+
 import '../../css/TakePicture.css';
 
 export default class TakePicture extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			thumbnailSpan: ''
+		}
+	}
+	componentDidMount() {
+		document.getElementById('file').addEventListener('change', (e) => {
+			handleFileSelect(e, this.setStateHelper.bind(this));
+			this.fileChanged();
+		}, false);
+	}
+	fileChanged() {
+		if(this.state.thumbnailSpan !== '') {
+			document.getElementById('thumbnailSpan').appendChild(this.state.thumbnailSpan);
+		}
+		else {
+			setTimeout(() => {
+				this.fileChanged();
+			}, 1000)
+		}
+
+		console.log(document.getElementById('thumbnailSpan'))
+	}
+	setStateHelper(stateToSet) {
+		this.setState(stateToSet);
+	}
 	render() {
 		return (
 			<div className='take-picture'>
 				<div className='tpic-child'>
 					<Text value='Take Picture' />
 					{' '}
-					<input type='file' accept='image/*' capture='camera' />
+					<input id='file' type='file' accept='image/*' capture='camera' />
 				</div>
 				<div className='tpic-child'>
 					<Text value='Choose Existing' />
 					{' '}
 					<input type='file' />
 				</div>
+				{this.state.thumbnailSpan !== '' &&
+					<div id='thumbnailSpan'></div>
+				}
 			</div>
 		)
 	}
---
+++ js/fileReader.js
@@ -0,0 +1,49 @@
+
+var fileReader = '';
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+  fileReader = true;
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+
+
+
+//<input type="file" id="files" name="files[]" multiple />
+//<output id="list"></output>
+
+  function handleFileSelect(evt, callback) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          // document.getElementById('list').insertBefore(span, null);
+          callback({ thumbnailSpan: span })
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+  }
+
+  // document.getElementById('').addEventListener('change', handleFileSelect, false);
+
+
+export { handleFileSelect, fileReader }
