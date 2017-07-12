dfd9d2d8f687b861182403ad5de2b07a8034da79
Coty Embry <cotyembry@gmail.com>
2017-07-11 23:13:23 -0500

    added more code

--- js/Components/CropOverlay.jsx
+++ js/Components/CropOverlay.jsx
@@ -7,33 +7,36 @@ import { uploadImage } from '../sendImageToServer.js';
 
 export default class CropOverlay extends React.Component {
 	addForm() {
-		// this works
+		// this renders an image to the screen
 		//
 		// var image = new Image();
 		// image.src = this.props.src;
-		let action = 'https://script.google.com/macros/s/AKfycbzITk9OTp4yOL3-TzRUnCEvXFccKreakinuR7LiVXArT4NE7IU/exec'
 
-		$(this.refs['formParent']).append(`
-			<div id="formContainer" style="display: none;"><!-- coty 05-25-2017 - since React Components are so weird when it comes to forms, I like using standard html for these types of elements...anyways, App.jsx uses this form to do a POST to my google script by creating an event and sending it to the below <button id="sendEmail">Send</button> element -->
-			  <form id="gform" method="POST" action="${action}">
-			    <fieldset>
-			      <textarea id="imgSrc" name="imgSrc" value="${this.props.src}"></textarea>
-			    </fieldset>
-			  </form>
-			</div>
-		`)
+
+
+		// $(this.refs['formParent']).append(`
+		// 	<div id="formContainer" style="display: none;"><!-- coty 05-25-2017 - since React Components are so weird when it comes to forms, I like using standard html for these types of elements...anyways, App.jsx uses this form to do a POST to my google script by creating an event and sending it to the below <button id="sendEmail">Send</button> element -->
+		// 	  <form id="gform" method="POST" action="${action}">
+		// 	    <fieldset>
+		// 	      <textarea id="imgSrc" name="imgSrc" value="${this.props.src}"></textarea>
+		// 	    </fieldset>
+		// 	  </form>
+		// 	</div>
+		// `)
 	}
 	componentWillMount() {
 		this.refs = [];
 	}
 	componentDidMount() {
 		this.addForm();
+
+		console.log(this.props.src)
 	}
 	submitForm() {
 		console.log('in this.submitForm')
 		
 
-		uploadImage('testPic.jpg', this.props.src, 'image/jpeg')
+		// uploadImage('testPic.jpg', this.props.src, 'image/jpeg')
 
 
 
@@ -46,6 +49,30 @@ export default class CropOverlay extends React.Component {
 		//   	console.log('in sucess: ', e)
 		//   },
 		//   dataType: 'jsonp'
+		// });
+
+		this.send();
+	}
+	send() {
+		console.log('in send')
+		let action = 'https://script.google.com/macros/s/AKfycbzITk9OTp4yOL3-TzRUnCEvXFccKreakinuR7LiVXArT4NE7IU/exec'
+
+
+		$.getJSON(action, function(response) {
+			console.log('response = ', response);
+		})
+		// $.ajax({
+		// 	type: 'POST',
+		// 	url: action,
+		// 	//data: {imgSrc: this.props.src},
+		// 	success: function(response) {
+		// 		console.log('in success. response = ', response)
+
+		// 	},
+		// 	complete: function() {
+		// 		console.log('in complete')
+		// 	},
+		// 	dataType: 'jsonp'
 		// });
 	}
 	render() {
