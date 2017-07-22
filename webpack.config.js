a2d407714af703c943a0c088f3b71e72462b4816
Coty Embry <cotyembry@gmail.com>
2017-07-22 11:28:39 -0500

    backup

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './js/index.js',
   output: {
@@ -19,5 +21,67 @@ module.exports = {
         test: /.css$/, loader: 'style-loader!css-loader'
       }
     ]
-  }
+  },
+  // plugins: [
+  //   // new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildExit:[_onBuildEnd]}),
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       NODE_ENV: JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin()
+  // ]
 }
+
+/*// //Current build path represents where the file needs to go to
+// const WebpackShellPlugin = require('webpack-shell-plugin');
+// var MOVE_FILE_TO = 'Y:/marsjs/js/build/MARSBPCHART/distribute';
+// var RECOMPILE_ON_THIS_FILE = MOVE_FILE_TO + '/bundle.js';
+// var shellCommand2 = 'mv ./dist/bundle.js ' + MOVE_FILE_TO;
+// var _onBuildEnd = shellCommand2;
+
+const webpack = require('webpack'); //this is used for a production build
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: __dirname,
+    filename: './dist/bundle.js'
+  },
+  module: {
+    loaders: [
+        {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              plugins: ['transform-runtime'],
+              presets: ['es2015', 'stage-0', 'react'],
+            }
+        },
+        {
+            test: /\.css$/,
+            loader: 'style-loader'
+        },
+        {
+            test: /\.css$/,
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+        }
+    ]
+  },
+  plugins: [
+    // new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildExit:[_onBuildEnd]}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
+
+*/
