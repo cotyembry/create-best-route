486d9c1501fe45b2a5071c302971c75b3d9809ff
cotyembry <cotyembry@gmail.com>
2018-03-26 03:15:35 -0400

    unstable changes but added logic to render a map link

--- web/js/Components/AddressList.jsx
+++ web/js/Components/AddressList.jsx
@@ -19,12 +19,14 @@ export default class ProcessPictures extends React.Component {
     componentWillReceiveProps(newProps) {
         console.log('newProps = ', newProps);
     }
-    render() {
+    render() { 
+        let _href = 'https://maps.google.com/?q=1200%20Pennsylvania%20Ave%20SE,%20Washington,%20District%20of%20Columbia,%2020003
         return (
             <View style={styles.ProcessPictures}>
                 <ScrollView style={{width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'space-between'}}>
                     {this.state.previousCoppedRects.map((addressText, i) =>
                         <View key={i}>
+                            <a href={_href}><Text>map link</Text></a>
                             <Text>{addressText[0].textFromCrop}</Text>
                         </View>
                     )}
